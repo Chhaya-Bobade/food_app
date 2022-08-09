1b2cbe272065a7b1fa3929dddde72eb6642bedf2
Chhaya-Bobade <91379325+Chhaya-Bobade@users.noreply.github.com>
2022-08-09 14:27:04 +0530

    Updates

---
+++ precache-manifest.6483749ee720679cf9761c065cdeecd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d6a2e186b07c293732",
+    "url": "/food_app/static/css/main.2f54c6cf.chunk.css"
+  },
+  {
+    "revision": "d9d6a2e186b07c293732",
+    "url": "/food_app/static/js/main.d9d6a2e1.chunk.js"
+  },
+  {
+    "revision": "1034e3dae8edae559f81",
+    "url": "/food_app/static/js/1.1034e3da.chunk.js"
+  },
+  {
+    "revision": "839d99bb0bd3b28b5625",
+    "url": "/food_app/static/js/runtime~main.839d99bb.js"
+  },
+  {
+    "revision": "66230f1a9941dd8769cafbac275f6ef5",
+    "url": "/food_app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/food-app/precache-manifest.af5354463b2c84e2a64f2a2279e5e6a6.js"
+  "/food_app/precache-manifest.6483749ee720679cf9761c065cdeecd9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/food-app/index.html", {
+workbox.routing.registerNavigationRoute("/food_app/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
